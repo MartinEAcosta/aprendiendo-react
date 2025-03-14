2cc26e0e1c95c7706e9ac51161d1ddb4a981ccc1
Martin <acostamartin1270@gmail.com>
2025-03-14 12:25:00 -0300

    finalizada clase de noteView y cascaron imageGallery

--- projects/journal-app/journal-app/src/journal/layout/JournalLayout.jsx
+++ projects/journal-app/journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material"
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -6,19 +7,21 @@ export const JournalLayout = ( { children } ) => {
     return (
         <Box sx={{ display: 'flex' }}>
 
-            {/* nav */}
+            <NavBar drawerWidth={ drawerWidth } />
 
-            {/* sidebar */}
+            <SideBar drawerWidth={ drawerWidth } />
 
             <Box 
                 component='main'
                 sx={{ flexGrow: 1 , p: 3}}
             >  
-                {/* ToolBar */}
+                <Toolbar />
 
                 { children }
 
             </Box>
+
+
 
         </Box>
     )
--- projects/journal-app/journal-app/src/journal/pages/JournalPage.jsx
+++ projects/journal-app/journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
 import { Typography } from "@mui/material";
 import { MailOutline } from '@mui/icons-material';
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
     return (
         <> 
             <JournalLayout>
-                <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum mollitia quae iure tenetur natus magni eaque itaque veritatis. Nihil ullam repellendus accusantium recusandae rem itaque dolore qui pariatur molestiae. Accusantium!</Typography>
+                {/* <NothingSelectedView /> */}
+                <NoteView />
             </JournalLayout>
         </>
     );
