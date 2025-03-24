43b52230933b58e58e707f24e4cff798a46804f7
Martin <acostamartin1270@gmail.com>
2025-03-24 17:05:18 -0300

    Finalización de sección  +animación de libreria

--- projects/journal-app/journal-app/src/auth/pages/LoginPage.jsx
+++ projects/journal-app/journal-app/src/auth/pages/LoginPage.jsx
@@ -37,7 +37,9 @@ export const LoginPage = () => {
 
     return (
       <AuthLayout title="Login">
-        <form onSubmit={ onSumbit }>
+        <form 
+          className="animate__animated animate__fadeIn animate__faster"
+          onSubmit={ onSumbit }>
           <Grid container>
             <Grid item xs={12} sx={{ mt: 2 }}>
               <TextField
--- projects/journal-app/journal-app/src/auth/pages/RegisterPage.jsx
+++ projects/journal-app/journal-app/src/auth/pages/RegisterPage.jsx
@@ -46,7 +46,9 @@ export const RegisterPage = () => {
 
     return (
       <AuthLayout title="Crear cuenta">
-        <form action="" onSubmit={ onSubmit } >
+        <form 
+          className="animate__animated animate__fadeIn animate__faster"
+          action="" onSubmit={ onSubmit } >
           <Grid container>
 
             <Grid item xs={12} sx={{ mt: 2 }}>
