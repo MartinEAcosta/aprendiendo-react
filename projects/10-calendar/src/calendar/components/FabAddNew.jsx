f5fd5f145aa8ed0d18fce47aaec13fd89a43b640
Martin <acostamartin1270@gmail.com>
2025-04-06 14:33:05 -0300

    se le da funcionalidad al botón fab

--- projects/10-calendar/src/calendar/components/FabAddNew.jsx
+++ projects/10-calendar/src/calendar/components/FabAddNew.jsx
@@ -1,8 +1,30 @@
+import { addHours } from "date-fns";
+import { useCalendarStore, useUiStore } from "../../hooks"
 
 
 export const FabAddNew = () => {
+
+    const { openDateModal } = useUiStore();
+    const { activeEvent , setActiveEvent } = useCalendarStore();
+
+    const onClickNew = () => {
+        setActiveEvent({
+            title: '',
+            notes: '',
+            start: new Date(),
+            end: addHours( new Date() , 2),
+            bgColort: '#fafafa',
+            user: {
+                _id: '123',
+                name: 'Martin'
+            }
+        });
+        openDateModal();
+    }
+
   return (
     <button 
+        onClick={ onClickNew }
         className="btn btn-primary fab"
     >
         <i className="fas fa-plus"></i>
