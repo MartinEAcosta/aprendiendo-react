446bdd61e1ff18b0bbd548033a14a5ab690cf2fe
Martin <acostamartin1270@gmail.com>
2025-04-09 15:25:24 -0300

    register con password hasheada, + login realizado por mi

--- projects/10-calendar-backend/controllers/auth.js
+++ projects/10-calendar-backend/controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario')
 
 
@@ -19,12 +20,16 @@ const crearUsuario = async( req , res = response ) => {
 
         usuario = new Usuario( req.body );
 
+        const salt = bcrypt.genSaltSync( );
+
+        usuario.password = bcrypt.hashSync( password , salt );
+
         await usuario.save();
         
-
         return res.status(201).json({
             ok: true,
-            msg: 'registro',
+            uid: usuario.id,
+            name: usuario.name,
         });
     }
     catch(error){
@@ -37,18 +42,41 @@ const crearUsuario = async( req , res = response ) => {
     }
 };
 
-const loginUsuario = ( req , res = response ) => {
+const loginUsuario = async( req , res = response ) => {
 
     const { email , password } = req.body;
 
+    try{
 
+        let usuario = await Usuario.findOne({ email });
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password,
-    });
+        console.log(usuario);
+        if ( usuario ){
+
+            const isPasswordLegit = bcrypt.compareSync( password , usuario.password  );
+
+            if( isPasswordLegit ){ 
+                return res.status(200).json({
+                    ok: true,
+                    msg: 'Logeado con exito',
+                    uid: usuario.id,
+                    name: usuario.name,
+                });
+            }
+
+        }
+
+        return res.status(400).json({
+            ok: false,
+            msg: 'Chequea las credenciales e intente nuevamente.'
+        });
+    }
+    catch(error){
+        return res.status(500).json({
+            ok: false,
+            msg: error,
+        });
+    }
 };
 
 const renovarToken = ( req , res  = response ) => {
