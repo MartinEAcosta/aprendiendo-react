afa5c70f8fca9c33ed97a8a9e8dc77e4fad24d64
Martin <acostamartin1270@gmail.com>
2025-04-16 17:13:53 -0300

    manteniendo esta de la autenticación

--- projects/10-calendar/src/api/calendarApi.js
+++ projects/10-calendar/src/api/calendarApi.js
@@ -7,4 +7,15 @@ const calendarApi = axios.create({
     baseURL: VITE_API_URL
 });
 
+// Configuración de interceptores
+calendarApi.interceptors.request.use( config => {
+    
+    config.headers = {
+        ...config.headers,
+        'x-token' : localStorage.getItem('token'),
+    }
+
+    return config;
+} )
+
 export default calendarApi;
--- projects/10-calendar/src/router/AppRouter.jsx
+++ projects/10-calendar/src/router/AppRouter.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { AuthPage } from '../auth';
 import { CalendarPage } from '../calendar';
+import { useAuthStore } from '../hooks/useAuthStore';
 
 export const AppRouter = () => {
 
-    const authStatus = 'not-authenticated';
+    const { status , checkAuthToken } = useAuthStore();
+
+    // const authStatus = 'not-authenticated';
+
+    useEffect(() => {
+        checkAuthToken();
+    }, [])
+    
+
+    if( status === 'checking' ) {
+        return(
+            <h3>Cargando...</h3>
+        )
+    }
 
     return (
         <Routes>
             {
-                ( authStatus === 'not-authenticated')
+                ( status === 'not-authenticated')
                 ?
                 <Route path='/auth/*' element={ <AuthPage /> } />
                 :
